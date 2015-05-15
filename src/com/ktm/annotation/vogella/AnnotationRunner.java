9ee379991eb9a9cc71396921be57f3503494d8ed
Rajan Prasad Upadhyay <rajanpupa@gmail.com>
2015-05-14 22:42:26 -1000

    added generics examples

--- src/com/ktm/annotation/vogella/AnnotationRunner.java
+++ src/com/ktm/annotation/vogella/AnnotationRunner.java
@@ -15,11 +15,17 @@ public class AnnotationRunner {
     public void method3() {
         System.out.println("method3");
     }
+    
+    @CanRun2
+    public void method7() {
+        System.out.println("method7");
+    }
 
     public void method4() {
         System.out.println("method4");
     }
 
+    @CanRun2
     public void method5() {
         System.out.println("method5");
     }
