cd8d709ab0c766b3b730610a419c2c16ebfdfbfd
Rajan Prasad Upadhyay <rajanpupa@gmail.com>
2015-05-31 16:37:00 -1000

    thread wait notify example added

---
+++ src/com/ktm/thread/wait/notify/WaitNotifyTest.java
@@ -0,0 +1,24 @@
+package com.ktm.thread.wait.notify;
+
+public class WaitNotifyTest {
+	 
+	/**
+	 * http://www.journaldev.com/1037/java-thread-wait-notify-and-notifyall-example
+	 * @param args
+	 */
+    public static void main(String[] args) {
+        Message msg = new Message("process it");
+        
+        Waiter waiter = new Waiter(msg);
+        new Thread(waiter,"waiter").start();
+         
+        Waiter waiter1 = new Waiter(msg);
+        new Thread(waiter1, "waiter1").start();
+         
+        Notifier notifier = new Notifier(msg);
+        new Thread(notifier, "notifier").start();
+        
+        System.out.println("\nAll the threads are started");
+    }
+ 
+}
