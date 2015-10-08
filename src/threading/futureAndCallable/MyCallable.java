514d2cdbe048e3d50ee09b58f049e926792f0a51
Rajan Prasad Upadhyay <rajanpupa@gmail.com>
2015-10-07 23:01:24 -0500

    threading examples and Java8 Stream examples added

---
+++ src/threading/futureAndCallable/MyCallable.java
@@ -0,0 +1,22 @@
+package futureAndCallable;
+
+import java.util.concurrent.Callable;
+
+/*
+ * Callable runs in different thread and returns a value as well (Generics)
+ * 
+ * Reference:
+ * http://www.vogella.com/tutorials/JavaConcurrency/article.html#futures
+ */
+
+public class MyCallable implements Callable<Long> {
+	@Override
+	public Long call() throws Exception {
+		long sum = 0;
+		for (long i = 0; i <= 100; i++) {
+			sum += i;
+		}
+		return sum;
+	}
+
+}
